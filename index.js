c0275e9c4afc1868cbfd21c7bcbf2c2153a7d94b
Austin Harlow <harlowaustin2@gmail.com>
2019-02-23 09:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-function happyHolidaysTo(name)() {
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo(name) {
+  return 'Happy holidays, ${name}!'
 }
-function happyHolidayTo(holiday, name)() {
-  return
+function happyHolidayTo(holiday, name) {
+  return 'Happy ${holiday}, ${name}!'
 }
-function holidayCountdown(days, holiday)() {
-  return
+function holidayCountdown(days, holiday) {
+  return 'It\'s ${days} days until ${holiday}!'
 }
